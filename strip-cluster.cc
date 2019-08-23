c531ff15db2caf12ef889bbbbaea5df9fd89f43c
Bei Wang <beiwang@princeton.edu>
2019-08-23 14:24:27 -0400

    seperate unpack and clusterizing stages

--- strip-cluster.cc
+++ strip-cluster.cc
@@ -6,6 +6,7 @@
 #include "FEDChannel.h"
 #include "FEDZSChannelUnpacker.h"
 
+/*
 class StripByStripAdder {
 public:
   typedef std::output_iterator_tag iterator_category;
@@ -40,13 +41,28 @@ OUT unpackZS(const FEDChannel& chan, uint16_t stripOffset, OUT out, detId_t idet
   auto unpacker = FEDZSChannelUnpacker::zeroSuppressedModeUnpacker(chan);
   while (unpacker.hasData()) {
     auto digi = SiStripDigi(stripOffset+unpacker.sampleNumber(), unpacker.adc());
-    //std::cout << "unpackZS det " << idet << " digi " << digi.strip() << " sample " << (unsigned int) unpacker.sampleNumber() << " adc " << (unsigned int) unpacker.adc() << std::endl;
+    //    std::cout << "unpackZS det " << idet << " digi " << digi.strip() << " sample " << (unsigned int) unpacker.sampleNumber() << " adc " << (unsigned int) unpacker.adc() << std::endl;
     if (digi.strip() != 0) {
       *out++ = digi;
     }
     unpacker++;
   }
   return out;
+}
+*/
+
+template<typename OUT>
+void unpackZS2(const FEDChannel& chan, uint16_t stripOffset, OUT& out, detId_t idet)
+{
+  auto unpacker = FEDZSChannelUnpacker::zeroSuppressedModeUnpacker(chan);
+  while (unpacker.hasData()) {
+    auto digi = SiStripDigi(stripOffset+unpacker.sampleNumber(), unpacker.adc());
+    //    std::cout << "unpackZS det " << idet << " digi " << digi.strip() << " sample " << (unsigned int) unpacker.sampleNumber() << " adc " << (unsigned int) unpacker.adc() << std::endl;
+    if (digi.strip() != 0) {
+      out.push_back(digi);
+    }
+    unpacker++;
+  }
 }
 
 FEDSet fillFeds()
@@ -58,7 +74,7 @@ FEDSet fillFeds()
 
   while (fedfile.read((char*)&detid, sizeof(detid)).gcount() == sizeof(detid)) {
     FEDChannel fed(fedfile);
-    //std::cout << "Det " << detid << " fed " << fed.fedId() << " channel " << (int) fed.fedCh() << " length " << fed.length() << std::endl;
+    //    std::cout << "Det " << detid << " fed " << fed.fedId() << " channel " << (int) fed.fedCh() << " length " << fed.length() << " offset " << fed.offset() << " ipair " << fed.iPair() << std::endl;
     feds[detid].push_back(std::move(fed));
   }
   return feds;
@@ -69,15 +85,26 @@ fillClusters(detId_t idet, Clusterizer& clusterizer, Clusterizer::State& state, 
 {
   static bool first = true;
   std::vector<SiStripCluster> out;
+  std::vector<SiStripDigi> input; 
 
+  // start clusterizing for idet
   auto const & det = clusterizer.stripByStripBegin(idet);
   state.reset(det);
 
+  // unpack RAW data to SiStripDigi
   for (auto const& chan : channels) {
-    //std::cout << "Processing channel for detid " << idet << " fed " << chan.fedId() << " channel " << (int) chan.fedCh() << " len:off " << chan.length() << ":" << chan.offset() << " ipair " << chan.iPair() << std::endl;
-    auto perStripAdder = StripByStripAdder(clusterizer, state, out);
-    unpackZS(chan, chan.iPair()*256, perStripAdder, idet);
+    //        std::cout << "Processing channel for detid " << idet << " fed " << chan.fedId() << " channel " << (int) chan.fedCh() << " len:off " << chan.length() << ":" << chan.offset() << " ipair " << chan.iPair() << std::endl;
+    //auto perStripAdder = StripByStripAdder(clusterizer, state, out);
+    //unpackZS(chan, chan.iPair()*256, perStripAdder, idet);
+    unpackZS2(chan, chan.iPair()*256, input, idet);
   }
+
+  // process each digi 
+  for (auto const& digi : input) {
+    clusterizer.stripByStripAdd(state, digi.strip(), digi.adc(), out);
+  }
+
+  // end clusterizing for idet
   clusterizer.stripByStripEnd(state, out);
 
   if (first) {
@@ -97,9 +124,11 @@ fillClusters(detId_t idet, Clusterizer& clusterizer, Clusterizer::State& state, 
 
 int main()
 {
+  //std::cout << "start initializing clusterizer" << std::endl;
   Clusterizer clusterizer;
   Clusterizer::State state;
 
+  //std::cout << "start fillFeds" <<std::endl;
   FEDSet feds(fillFeds());
   for (auto idet : clusterizer.allDetIds()) {
     if (feds.find(idet) != feds.end()) {
